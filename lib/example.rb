97de814c60bb39673ccc4fd1855cf6b4284c6858
Jeong Park <wjdtn1220@gmail.com>
2019-08-12 15:40:05 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,7 @@
 # Use this space to code along with the readme
 
+def say_hello_ten_times
+  
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +13,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
